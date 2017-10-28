a17377b45dbcf73b94007e1bb3a05aec1884f96c
Alonso Avotz <alonso@avotz.com>
2017-10-27 18:46:46 -0600

    se cambio mapa en contact

--- wp-content/themes/cocotours/Gulpfile.js
+++ wp-content/themes/cocotours/Gulpfile.js
@@ -24,7 +24,7 @@ var gulp        = require('gulp'),
 
 gulp.task('js', function () {
   gulp.src([
-      './assets/js/vendor/jquery-1.11.2.min.js',
+      /*'./assets/js/vendor/jquery-1.11.2.min.js',*/
       './assets/js/vendor/jquery.hoverIntent.minified.js',
       /*'./assets/js/vendor/jquery.contentcarousel.js',*/
       //'./assets/js/vendor/jquery.easing.1.3.js',
--- wp-content/themes/cocotours/template-parts/content-contact.php
+++ wp-content/themes/cocotours/template-parts/content-contact.php
@@ -24,7 +24,7 @@
                 ?>
             </div>
             <div class="contact-media">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.466378756392!2d-85.69433198478445!3d10.542630166347337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f9e2978ffdfebd3%3A0x22ca60648e0222c!2s151%2C+Provincia+de+Guanacaste%2C+Coco!5e0!3m2!1sen!2scr!4v1507066065684" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15684.919025455205!2d-85.4281962!3d10.639251!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x756af3ebd14361a2!2sAvotz+Web+Works!5e0!3m2!1ses-419!2s!4v1442615525574" width="600" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
             </div>
         </div>
         
